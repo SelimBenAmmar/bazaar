135c60b46d90ecdf005d3f03b8036029ea7d9a27
Blabla <hackacka@localhost.localdomain>
2017-03-12 01:43:00 +0000

    first commit

---
+++ 1.js
@@ -0,0 +1,21 @@
+function insertPoints () 
+{
+	var points ="500" ;
+    document.getElementById('pointsT').innerHTML = points + " points";
+}
+
+function insertFamilyName () 
+{
+	var familyName ="AMMARIA" ;
+    document.getElementById('familyName').innerHTML = familyName ;
+}
+
+function insertName () 
+{
+	name1 = "Selim" ;
+	name2 = "Manar" ;
+    document.getElementById("name1").innerHTML = name1 ;
+    document.getElementById("name2").innerHTML = name2 ;
+}
+
+		
