4127ac07a1cd4ce45bf208dc284d7cf9d66248a6
Blabla <hackacka@localhost.localdomain>
2017-03-12 04:14:29 +0000

    first commit

---
+++ refugee.js
@@ -0,0 +1,17 @@
+function insertFamilyName ()
+{
+	var familyName ="AMMAR" ;
+    document.getElementById('familyName').innerHTML = familyName ;
+}
+
+function insertName ()
+{
+	name1 = "Selim" ;
+	name2 = "Manar" ;
+	name3 = "Meriem" ;
+	name4 = "Family" ;
+  document.getElementById("name1").innerHTML = name1 ;
+  document.getElementById("name2").innerHTML = name2 ;
+	document.getElementById("name3").innerHTML = name3 ;
+	document.getElementById("name4").innerHTML = name4 ;
+}
